4b1b7e19742725edef0cefbee4d6bfe7b688d88e
shane-smyth <shanes3210@gmail.com>
2025-11-03 10:41:20 +0000

    test(iot): created a subscriber to test publisher
    created a pubnub subscriber file that prints out messages published by pubnub_client.py

--- hardware/pubnub_subscriber_test.py
+++ hardware/pubnub_subscriber_test.py
@@ -1,7 +1,6 @@
-# https://www.pubnub.com/docs/sdks/python
-
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
+from pubnub.callbacks import SubscribeCallback
 from dotenv import load_dotenv
 import os
 
@@ -14,13 +13,15 @@ pnconfig.uuid = os.getenv("PUBNUB_UUID")
 pnconfig.ss1 = True
 
 pubnub = PubNub(pnconfig)
-
 CHANNEL = os.getenv("PUBNUB_CHANNEL")
 
-def publish_message(message):
-    envelope = pubnub.publish().channel(CHANNEL).message(message).sync()
+class IDListener(SubscribeCallback):
+    def message(self, pubnub, message):
+        print(f"{message.message}")
 
-    if envelope.status.is_error():
-        print(f'Publish failed: {e}')
-    else:
-        print(f'Published message with timetoken: {envelope.result.timetoken}')
+
+pubnub.add_listener(IDListener())
+pubnub.subscribe().channels(CHANNEL).execute()
+
+while True:
+    pass
