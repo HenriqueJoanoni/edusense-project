90816e99c415f24a5129ecceebc3ab55c0d64613
chealy90 <christopherhealy20@gmail.com>
2025-11-12 09:46:48 +0000

    feat: Create class groups page
    Created page to display the groups each user is a part of and connect them to that page

--- frontend/src/components/ClassGroupCard.jsx
+++ frontend/src/components/ClassGroupCard.jsx
@@ -11,7 +11,7 @@ export default function ClassGroupCard(props){
                 </div>
                 
             </div>
-            <a href="#">Expand</a>
+            <a href={props.linkTo}>Expand</a>
         </div>
     )
 }
--- frontend/src/pages/ClassOverview.jsx
+++ frontend/src/pages/ClassOverview.jsx
@@ -1,10 +1,33 @@
 import React, {useState, useEffect} from "react"
+import axios from "axios"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import ClassGroupCard from "../components/ClassGroupCard"
 import "../css/ClassOverview.css"
+import {SERVER_ADDRESS} from "../config/global_constants"
 
 export default function ClassOverview(){
+    const accessLevelMap = {
+        1: "/student/attendance/",
+        2: "/teacher/attedance/",
+        3: "admin/classGroup/"
+    }
+
+    /*const [userGroups, setUserGroups] = useState([])
+
+    useEffect(()=>{
+        axios.get(`${SERVER_ADDRESS}/TODO`
+            .then(res => {
+                if (res.status == 200){
+                    setUserGroups(#)
+                }
+            })
+        )
+    }, [])
+
+    */
+
+
     const userGroups = [
         {
             id: "axc1123",
@@ -47,7 +70,10 @@ export default function ClassOverview(){
                 <div id="classOverviewMain">
                     <h2>Your Class Groups</h2>
                     <div id="classList">
-                        {userGroups.map(group => <ClassGroupCard classGroup={group}/>)}
+                        {userGroups.map(group => {
+                            const linkTo = accessLevelMap[localStorage.getItem("accessLevel")] + group.id
+                            return <ClassGroupCard classGroup={group} linkTo={linkTo}/>}
+                        )}
                     </div>
                 </div>
             </div>
